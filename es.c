8557caca77fd554946dffd190a1899ea2b79a87b
Thomas LECLERE <thomas.leclere@centrale-marseille.fr>
2018-02-02 10:40:30 +0100

    Kitarsh : fin de l'écriture de la fonction "esSaisir"

--- es.c
+++ es.c
@@ -13,7 +13,7 @@ void esSaisir(T_Mat *pMat)
     //attribution des variables
     //-------------------------
     int nbLig, nbCol;
-    double valTmp
+    double valTmp;
     //
     //-------------------------------------------------
     //demander la taille de la matrice (Ligne, Colonne)
@@ -48,24 +48,86 @@ void esSaisir(T_Mat *pMat)
                     printf("Mat[%d,%d] = ", lig, col);
                     scanf("%lg",&valTmp);
                     printf("\n\n");
-                    pMat[i][j]
+                    pMat->Elts[col + lig * nbCol] = valTmp;
                 }
             }
             break;
         case diag:
+            for(int lig = 0; lig < nbLig; lig++)
+            {
+                printf("Mat[%d,%d] = ", lig, lig);
+                scanf("%lg",&valTmp);
+                printf("\n\n");
+                pMat->Elts[lig + lig * nbCol] = valTmp;
+            }
             break;
         case tridiag:
+            for(int lig = 0; lig < nbLig; lig++)
+            {
+                if(lig != 0){   
+                    printf("Mat[%d,%d] = ", lig , lig - 1);
+                    scanf("%lg",&valTmp);
+                    printf("\n\n");
+                    pMat->Elts[lig - 1 + lig * nbCol] = valTmp;
+                }
+                printf("Mat[%d,%d] = ", lig, lig);
+                scanf("%lg",&valTmp);
+                printf("\n\n");
+                pMat->Elts[lig + lig * nbCol] = valTmp;
+                if(lig != nbLig){
+                    printf("Mat[%d,%d] = ", lig , lig + 1);
+                    scanf("%lg",&valTmp);
+                    printf("\n\n");
+                    pMat->Elts[lig + 1 + lig * nbCol] = valTmp;
+                }
+            }
             break;
         case triinf:
+            for(int lig = 0; lig < nbLig; lig++)
+            {
+                for(int col = 0; col < nbCol; col++)
+                {
+                    if(lig >= col){
+                        printf("Mat[%d,%d] = ", lig, col);
+                        scanf("%lg",&valTmp);
+                        printf("\n\n");
+                        pMat->Elts[col + lig * nbCol] = valTmp;
+                    }
+                }
+            }
             break;
         case trisup:
+            for(int lig = 0; lig < nbLig; lig++)
+            {
+                for(int col = 0; col < nbCol; col++)
+                {
+                    if(lig <= col){
+                        printf("Mat[%d,%d] = ", lig, col);
+                        scanf("%lg",&valTmp);
+                        printf("\n\n");
+                        pMat->Elts[col + lig * nbCol] = valTmp;
+                    }
+                }
+            }
             break;
         case sym:
+            for(int lig = 0; lig < nbLig; lig++)
+            {
+                for(int col = 0; col < nbCol; col++)
+                {
+                    if(lig <= col){
+                        printf("Mat[%d,%d] = ", lig, col);
+                        scanf("%lg",&valTmp);
+                        printf("\n\n");
+                        pMat->Elts[col + lig * nbCol] = valTmp;
+                        if((lig != col) && ((lig + col * nbCol) < (nbLig * nb Col))){
+                            pMat->Elts[lig + col * nbCol] = valTmp;
+                        }
+                    }
+                }
+            }
             break;
-        default:
-            //faut-il écrire quelque chose ?
     }
-
 }
 
 void esAfficher(T_Mat *pMat)
