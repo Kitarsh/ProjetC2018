d5e057d865552d8b8157eb79699939b9c8ea1995
Melle Charles <cmelle@gaby220.etu.ec-m.fr>
2018-02-09 08:13:54 +0100

    Charles : err.c : erreur 5

--- err.c
+++ err.c
@@ -8,7 +8,8 @@ void errMsg(int num)
                  "erreur 1: Les deux matrices n'ont pas les memes dimensions",
                  "erreur 2: le nombre de colonnes de la 1ere matrice est different du nombre de lignes de la 2eme",
                  "erreur 3: La matrice n'est pas carre",
-                 "erreur 4: Le vecteur B n'est pas de bonne dimension"};
+                 "erreur 4: Le vecteur B n'est pas de bonne dimension",
+                 "erreur 5: Le pivot est nul"};
   
   printf("%d : %s\n", num, Msg[num]);
   exit (1);
