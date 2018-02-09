c3bdba059e4059a8654f39b7a4527bed8021a9d3
Thomas Leclère <thomas.leclere@centrale-marseille.fr>
2018-02-09 08:08:50 +0100

    Kitarsh : Ajout d'une erreur pour le vecteur B

--- err.c
+++ err.c
@@ -7,7 +7,8 @@ void errMsg(int num)
   char *Msg[] = {" tout va bien ",
                  "erreur 1: Les deux matrices n'ont pas les memes dimensions",
                  "erreur 2: le nombre de colonnes de la 1ere matrice est different du nombre de lignes de la 2eme",
-                 "erreur 3: La matrice n'est pas carre"};
+                 "erreur 3: La matrice n'est pas carre",
+                 "erreur 4: Le vecteur B n'est pas de bonne dimension"};
   
   printf("%d : %s\n", num, Msg[num]);
   exit (1);
